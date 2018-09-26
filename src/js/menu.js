02ccc43c5df9fa4dca6494dafb71bcd91cb7edb4
remzov <remzovnik@gmail.com>
2018-09-26 14:39:52 +0300

    'before-integration'

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,19 +1,20 @@
 import TimelineMax from 'gsap/src/uncompressed/TimelineMax';
 
 export default function(menuBtn, menu) {
+
+    const showMenu = new TimelineMax();
+
     menuBtn.addEventListener('click', () => {
         
         menuBtn.classList.toggle('menu-btn_isOpened');
         menu.classList.toggle('main-nav_isOpened');
 
         if (menu.classList.contains('main-nav_isOpened')) {
-            const showMenu = new TimelineMax();
             showMenu.fromTo('.menu-btn__bg', 1, {scale: 1}, {scale: 60});
-            showMenu.fromTo('.menu-btn__bg', 0.1, {opacity: 1}, {opacity: 0}, '-=0.25');
+            showMenu.fromTo('.menu-btn__bg', 0.1, {opacity: 1, scale: 60}, {opacity: 0, scale: 1}, '-=0.25');
             showMenu.fromTo('.main-nav', 0.1, {opacity: 0}, {opacity: 1}, '-=0.35');
             showMenu.staggerFromTo('.main-nav li', 0.25, {y: -30, opacity: 0}, {y: 0, opacity: 1}, 0.25, '-=0.3');
         }
-       
     });
 }
 
