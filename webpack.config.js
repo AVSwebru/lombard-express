490ce07111e9d82f70558c0f6962078dcf203981
Nikita Remzov <remz@MacBook-Pro-Nikita.local>
2019-02-25 10:21:46 +0300

    restructure

--- webpack.config.js
+++ webpack.config.js
@@ -3,115 +3,112 @@ const path = require('path');
 const env = process.env.WEBPACK_ENV;
 
 const PATHS = {
-  source: path.join(__dirname, 'src/'),
-  build: path.join(__dirname, 'dist/')
+  source: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'build')
 };
 
 module.exports = {
   entry: {
-    main: PATHS.source + 'index.js'
+    main: PATHS.source + '/index.js'
   },
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
+  devtool: 'source-map',
   devServer: {
-    index: 'index.html',
-    compress: true,
     port: 9000,
+    compress: true,
+    open: true,
+    overlay: true,
+    contentBase: PATHS.build
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    },
-    {
-      test: /\.(scss)$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: 'main.css'
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'css/main.css'
+            }
+          },
+          {
+            loader: 'extract-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              minimize: env === 'production' ? true : false,
+              url: false
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('precss'),
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
           }
-        },
-        {
-          loader: 'extract-loader',
-        },
-        {
-          loader: 'css-loader',
-          options: {
-            url: false,
-            minimize: true,
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          },
+          {
+            loader: 'extract-loader'
+          },
+          {
+            loader: 'html-loader'
           }
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            plugins: function() {
-              return [
-                require('precss'),
-                require('autoprefixer')
-              ];
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|webm|mp4|ogg|ogv)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]?[hash]',
+              outputPath: 'img/'
             }
           }
-        },
-        {
-          loader: 'sass-loader',
-        }
-      ]
-    },
-    {
-      test: /\.css$/,
-      use: [
-        {
-          loader: 'sass-loader',
-          options: {
-            minimize: env === 'production' ? true : false
+        ]
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]?[hash]',
+              outputPath: 'fonts/'
+            }
           }
-        },
-        {
-          loader: 'postcss-loader'
-        }
-      ]
-    },
-    {
-      test: /\.html$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]'
-          }
-        },
-        {
-          loader: 'extract-loader'
-        },
-        {
-          loader: 'html-loader'
-        }
-      ]
-    },
-    {
-      test: /\.(png|jpg|gif|svg|webm|mp4|ogg|ogv)/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]',
-          outputPath: 'img/'
-        }
-      }]
-    },
-    {
-      test: /\.(eot|woff|woff2|ttf|otf)$/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]',
-          outputPath: 'fonts/'
-        }
-      }]
-    }
+        ]
+      }
     ]
   },
   resolve: {
@@ -126,13 +123,6 @@ module.exports = {
     }
   },
   plugins: [
-    new webpack.DefinePlugin({
-      BUNDLED: true,
-      VERSION: '1.0',
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    })
+    new webpack.HotModuleReplacementPlugin()
   ]
-
 };
